ef7f7986aa13551d8e6a8570472ef4575762de2f
nathanylee <leen21@uw.edu>
2016-03-10 15:49:07 -0800

    choose group and month select commented

--- choose_call_group.R
+++ choose_call_group.R
@@ -4,6 +4,7 @@ library(shiny)
 library(dplyr)
 library(leaflet)
 
+# A function that selects a group within our dataset of 911 Calls in 2012, and filters the data to show only the calls that are categorized in that group
 choose_call_group <- function(group) {
   chosen_group <- only_2012 %>% filter(Event.Clearance.Group == group)
   return(chosen_group)
